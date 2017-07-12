c268ec608b5327a79b821d0b8ce0b47181b73a39
michaellennox <michaelctlennox@gmail.com>
2017-07-12 13:04:06 +0300

    Implement a seed for the grand alliances
    In order to ensure alliances are correctly entered into the database
    they are inserted using the Seeds::CreateGrandAlliances service which is
    run via the seeds:create_grand_alliances rake task.

---
+++ spec/db/seeds/create_grand_alliances_spec.rb
@@ -0,0 +1,56 @@
+require 'spec_helper'
+require_relative '../../../db/seeds/create_grand_alliances'
+
+RSpec.describe Seeds::CreateGrandAlliances do
+  let(:repository) { instance_double(GrandAllianceRepository) }
+  before do
+    allow(repository).to receive(:find)
+    allow(repository).to receive(:create)
+  end
+
+  subject(:service) { described_class.new(repository) }
+
+  context 'when all grand alliances already exist' do
+    before do
+      allow(repository).to receive(:find).with('Order').and_return(instance_double GrandAlliance)
+      allow(repository).to receive(:find).with('Chaos').and_return(instance_double GrandAlliance)
+      allow(repository).to receive(:find).with('Destruction').and_return(instance_double GrandAlliance)
+      allow(repository).to receive(:find).with('Death').and_return(instance_double GrandAlliance)
+    end
+
+    it 'creates no grand alliance records' do
+      service.call
+
+      expect(repository).not_to have_received(:create)
+    end
+  end
+
+  context 'when some grand alliances already exist' do
+    before do
+      allow(repository).to receive(:find).with('Order').and_return(instance_double GrandAlliance)
+      allow(repository).to receive(:find).with('Chaos').and_return(instance_double GrandAlliance)
+    end
+
+    it 'creates the missing grand alliance records' do
+      service.call
+
+      aggregate_failures do
+        expect(repository).to have_received(:create).with(name: 'Destruction')
+        expect(repository).to have_received(:create).with(name: 'Death')
+      end
+    end
+  end
+
+  context 'when no grand alliances already exist' do
+    it 'creates all the grand alliance records' do
+      service.call
+
+      aggregate_failures do
+        expect(repository).to have_received(:create).with(name: 'Order')
+        expect(repository).to have_received(:create).with(name: 'Chaos')
+        expect(repository).to have_received(:create).with(name: 'Destruction')
+        expect(repository).to have_received(:create).with(name: 'Death')
+      end
+    end
+  end
+end
