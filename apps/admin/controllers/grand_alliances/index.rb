6864f354c0f6321b73e42453f65bf66e28a27ef5
michaellennox <michaelctlennox@gmail.com>
2017-07-12 13:41:33 +0300

    The GrandAlliances::Index controller exposes the list of grand alliances

--- apps/admin/controllers/grand_alliances/index.rb
+++ apps/admin/controllers/grand_alliances/index.rb
@@ -2,7 +2,10 @@ module Admin::Controllers::GrandAlliances
   class Index
     include Admin::Action
 
+    expose :grand_alliances
+
     def call(params)
+      @grand_alliances = GrandAllianceRepository.new.all
     end
   end
 end
